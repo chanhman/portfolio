e87d9c31dc2850393391a7a62859fe2c6d247eca
Chanh Nguyen <chanhnguyen@Chanhs-MacBook-Pro.local>
2020-07-11 15:02:45 -0600

    Attempt 2

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,22 +2,29 @@ import Head from 'next/head'
 import Hero from '../components/Hero'
 import CaseStudy from '../components/CaseStudy'
 
+const GA_TRACKING_ID = 'UA-172462762-1'
+
 export default function Home() {
   return (
     <>
       <Head>
-        {/* Global site tag (gtag.js) - Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-172462762-1"></script>
-        <script>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments)}
-              gtag('js', new Date());
-              gtag('config', 'UA-172462762-1');
-            `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
           }}
-        </script>
+        />
 
         <title>Chanh Nguyen</title>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Roboto&display=swap" rel="stylesheet"/> 
