f340dcaa495b908fe71ca79d6ee2a621ae2bfeb2
Chanh Nguyen <chanhthedj@gmail.com>
2020-09-16 22:12:25 -0600

    Create a page for each case study

---
+++ pages/case-studies/built-for-teams.jsx
@@ -0,0 +1,10 @@
+import Layout from 'components/layout'
+import BuiltForTeams from 'components/BuiltForTeams'
+
+const BuiltForTeamsCaseStudy = () => (
+  <Layout>
+    <BuiltForTeams/>
+  </Layout>
+)
+
+export default BuiltForTeamsCaseStudy
---
+++ pages/case-studies/gear-trade.jsx
@@ -0,0 +1,10 @@
+import Layout from 'components/layout'
+import GearTrade from 'components/GearTrade'
+
+const GearTradeCaseStudy = () => (
+  <Layout>
+    <GearTrade/>
+  </Layout>
+)
+
+export default GearTradeCaseStudy
