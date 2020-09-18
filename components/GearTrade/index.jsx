d8eddaf35caf745de45d1ca476b9c16ab94bfa98
Chanh Nguyen <chanhthedj@gmail.com>
2020-09-17 20:31:30 -0600

    add images to GearTrade

--- components/GearTrade/index.jsx
+++ components/GearTrade/index.jsx
@@ -18,21 +18,20 @@ const GearTrade = () => (
       description="Google Analytics was used to view the workflow before the redesign. Working with the client, we located the friction points and decided what needed to be improved before the release, and what could be reworked afterward."
     >
       <img src="http://placekitten.com/g/200/300" alt=""/>
-      <img src="http://placekitten.com/g/200/300" alt=""/>
     </ProjectTask>
 
     <ProjectTask
       name="Workflows and Prototyping"
       description="The client’s first priority was to improve the workflow to create a post. The goal was to remove friction for the user."
     >
-      <img src="http://placekitten.com/g/300/200" alt=""/>
+      <img src="/projects/gear-trade__wireframes-zoomed-in.png" alt=""/>
+      <img src="/projects/gear-trade__wireframes-zoomed-out.png" alt=""/>
     </ProjectTask>
 
     <ProjectTask
       name="Design"
       description="The website needed an updated look to help create more confidence for their customers. A minimalist design was implemented to make sure the products stood out."
     >
-      <img src="http://placekitten.com/g/200/300" alt=""/>
       <img src="http://placekitten.com/g/200/300" alt=""/>
     </ProjectTask>
   </section>
