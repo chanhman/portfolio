4591c0c5ad39eeaf5b04ee8e11bd084483d8a6a9
Chanh Nguyen <chanhthedj@gmail.com>
2020-10-13 23:57:39 -0600

    Swap job titles

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -6,7 +6,7 @@ const Hero = () => (
     <div className={styles.inner}>
       <h1 className={styles.title}>Chanh Nguyen</h1>
       <p className={styles.pronunciation}>/chan win/</p>
-      <p className={styles.description}>I am a designer and front-end developer at <a href="https://www.objectiveinc.com/">Objective</a> in Salt Lake City. Contact me at <a href="mailto:hello@chanh.design">hello@chanh.design</a> if you’d like to collaborate.</p>
+      <p className={styles.description}>I am a front-end developer and designer at <a href="https://www.objectiveinc.com/">Objective</a> in Salt Lake City. Contact me at <a href="mailto:hello@chanh.design">hello@chanh.design</a> if you’d like to collaborate.</p>
       <SocialMediaLinks/>
     </div>
   </section>
