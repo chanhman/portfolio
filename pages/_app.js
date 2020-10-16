15caaae22137595723e851699fdb5da8bf201146
Chanh Nguyen <chanhthedj@gmail.com>
2020-10-15 22:22:18 -0600

    Add page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
-import '../scss/normalize.scss'
+import { motion } from 'framer-motion';
 
-import '../scss/_a.scss'
-import '../scss/_body.scss'
-import '../scss/_font-family.scss'
-import '../scss/_html.scss'
-import '../scss/_h1-h6.scss'
-import '../scss/_box-sizing.scss'
-import '../scss/_p.scss'
+import '../scss/normalize.scss';
+import '../scss/_a.scss';
+import '../scss/_body.scss';
+import '../scss/_font-family.scss';
+import '../scss/_html.scss';
+import '../scss/_h1-h6.scss';
+import '../scss/_box-sizing.scss';
+import '../scss/_p.scss';
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps, router }) {
+  return (
+    <motion.div
+      key={router.route}
+      initial='pageInitial'
+      animate='pageAnimate'
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 }
--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,31 +1,10 @@
-import { motion } from 'framer-motion'
 import Layout from 'components/Layout'
 import Resume from 'components/Resume'
 
 const ResumePage = () => {
-  const motionVariants = {
-    hidden: {
-      opacity: 0,
-      y: -30
-    },
-    visible: {
-      opacity: 1,
-      transition: {
-        delay: .5
-      },
-      y: 0
-    },
-  }
-
   return (
     <Layout>
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={motionVariants}
-      >
-        <Resume />
-      </motion.div>
+      <Resume />
     </Layout>
   )
 }
