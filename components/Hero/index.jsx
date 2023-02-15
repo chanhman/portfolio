e87ac0e9b65818d144c74c5b3a363f99e8915d9b
chanhman <hello@chanh.design>
2023-02-14 17:55:12 -0700

    Update recent job title on the home page.

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
       >
         <h1 className={styles.title}>Chanh Nguyen</h1>
         <p className={styles.pronunciation}>/chan win/</p>
-        <p className={styles.description}>I am a designer and front-end developer at <a href="https://www.objectiveinc.com/">Objective</a> in Salt Lake City. Contact me at <a href="mailto:hello@chanh.design">hello@chanh.design</a> if you’d like to collaborate.</p>
+        <p className={styles.description}>I am a software engineer at <a href="https://www.weedmaps.com/">Weedmaps</a> in Salt Lake City. Contact me at <a href="mailto:hello@chanh.design">hello@chanh.design</a> if you’d like to collaborate.</p>
 
         <SocialMediaLinks />
       </motion.div>
